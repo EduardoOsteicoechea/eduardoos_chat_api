53cca454f2b7d95a7352f4f6c003366ed55c366f
eduar <eduar@EDUARDO>
2025-05-17 07:07:05 -0400

    test deploy

--- Program.cs
+++ Program.cs
@@ -18,41 +18,46 @@ app.MapGet("/chatbot", () => "Hello World!");
 
 app.MapPost("/chatbot/about/eduardo", async context =>
 {
-		try
-		{
-			Console.WriteLine($"Starting request processing at: {DateTime.Now}");
+  try
+  {
+    Console.WriteLine($"Starting request processing at: {DateTime.Now}");
 
-			SimpleMessagingChatRequest? request = await context.Request.ReadFromJsonAsync<SimpleMessagingChatRequest>();
-			
-			Console.WriteLine($"After deserialization:");
-			if (request!.previous_messages == null)
-			{
-				Console.WriteLine("PreviousMessages is null.");
-			}
-			else if (request.previous_messages.Length == 0)
-			{
-				Console.WriteLine("PreviousMessages is an empty array.");
-			}
-			else
-			{
-				Console.WriteLine($"PreviousMessages count: {request.previous_messages.Length}");
-				foreach (var prevMessage in request.previous_messages)
-				{
-					Console.WriteLine($"  Role: {prevMessage.Role}, Content: {prevMessage.Content}");
-				}
-			}
+    SimpleMessagingChatRequest? request = await context.Request.ReadFromJsonAsync<SimpleMessagingChatRequest>();
 
-			Console.WriteLine($"Message - Role: {request.message?.Role}, Content: {request.message?.Content}");
-			
-			string aboutEduardoChatResponse = await new AboutEduardoChatResponseManager().GetResponse(request);
-			Console.WriteLine($"Message Before Api Return: {aboutEduardoChatResponse}");
+    Console.WriteLine($"After deserialization:");
+    if (request!.previous_messages == null)
+    {
+      Console.WriteLine("PreviousMessages is null.");
+    }
+    else if (request.previous_messages.Length == 0)
+    {
+      Console.WriteLine("PreviousMessages is an empty array.");
+    }
+    else
+    {
+      Console.WriteLine($"PreviousMessages count: {request.previous_messages.Length}");
+      foreach (var prevMessage in request.previous_messages)
+      {
+        Console.WriteLine($"  Role: {prevMessage.Role}, Content: {prevMessage.Content}");
+      }
+    }
 
-			Results.Text(aboutEduardoChatResponse);
-		}
-		catch (System.Exception exception)
-		{
-			Results.BadRequest(exception.Message);
-		}
+    Console.WriteLine($"Message - Role: {request.message?.Role}, Content: {request.message?.Content}");
+
+    // string aboutEduardoChatResponse = await new AboutEduardoChatResponseManager().GetResponse(request);
+    // Console.WriteLine($"Message Before Api Return: {aboutEduardoChatResponse}");
+
+    // Results.Text(aboutEduardoChatResponse);
+
+    string hardcodedResponse = "This is a hardcoded profile response.";
+    Console.WriteLine($"Message Before Api Return: {hardcodedResponse}");
+
+    Results.Text(hardcodedResponse, contentType: "application/text");
+  }
+  catch (System.Exception exception)
+  {
+    Results.BadRequest(exception.Message);
+  }
 });
 
 app.UseCors("AllowLocalhostReact");
