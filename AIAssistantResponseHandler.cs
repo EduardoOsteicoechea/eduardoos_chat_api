a03573a6b1ca5c04c1d814dade7c85b6184135c9
eduar <eduar@EDUARDO>
2025-05-17 07:01:33 -0400

    test deploy

--- AIAssistantResponseHandler.cs
+++ AIAssistantResponseHandler.cs
@@ -31,7 +31,7 @@ public static class AIAssistantResponseHandler
 			}
 
 			Console.WriteLine($"Message - Role: {request.message?.Role}, Content: {request.message?.Content}");
-			context.Response.ContentType = "application/text";
+			// context.Response.ContentType = "application/text";
 			
 			string aboutEduardoChatResponse = await responseManager.GetResponse(request);
 			Console.WriteLine($"Message Before Api Return: {aboutEduardoChatResponse}");
