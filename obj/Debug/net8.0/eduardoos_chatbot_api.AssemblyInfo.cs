8614b0180fb0ececd78c1eaebe52240f26fb6657
eduar <eduar@EDUARDO>
2025-06-21 05:12:52 -0400

    uploading files

--- obj/Debug/net8.0/eduardoos_chatbot_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/eduardoos_chatbot_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eduardoos_chatbot_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e8a0874ed3ad08284fc7cdecdec1c1a56a3bdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ce978bc4f0db38962f0b2bfa9660890f0e8f05")]
 [assembly: System.Reflection.AssemblyProductAttribute("eduardoos_chatbot_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eduardoos_chatbot_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
